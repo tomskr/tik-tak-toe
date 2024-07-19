06fa2b1fc0da36b74aff2406a5eac8489821c85a
tomskr <33270937+tomskr@users.noreply.github.com>
2024-07-19 09:23:30 +0000

    Set useState on Board

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
+import { useState } from "react";
 
-function Square({value}){
-  return <button className="square">{value}</button>;
+
+function Square(){
+  return (<button 
+            className="square"
+          >
+            {value}
+          </button>);
 }
 
 export default function Board(){
+  const [squares, setSquares] = useState(Array(9).fill(null));
+
   return(
   <>
     <div className="board-row">
-      <Square value="1"/>
-      <Square value="2"/>
-      <Square value="3"/>
+      <Square value = {"squares[0]"}/>
+      <Square value = {"squares[1]"}/>
+      <Square value = {"squares[2]"}/>
     </div>
     <div className="board-row">
-      <Square value="4"/>
-      <Square value="5"/>
-      <Square value="6"/>
+      <Square value = {"squares[3]"}/>
+      <Square value = {"squares[4]"}/>
+      <Square value = {"squares[5]"}/>  
     </div>
     <div className="board-row">
-      <Square value="7"/>
-      <Square value="8"/>
-      <Square value="9"/>
+      <Square value = {"squares[6]"}/>  
+      <Square value = {"squares[7]"}/>  
+      <Square value = {"squares[8]"}/>  
     </div>
   </>
   );
